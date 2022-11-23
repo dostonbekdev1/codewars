da076171c9f87dc81f54507313e56b286c82b9ef
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-23 09:47:59 +0500

    return a string

---
+++ 18-11/app.js
@@ -0,0 +1,19 @@
+// function greet(name){
+//your code here
+//  return `Hello, ${name} how are you doing today?`
+// }
+//    console.log(greet("Sardor"));
+
+
+   // Return a string of the number here!
+
+  //  function numberToString(num) {
+  //   return String(num)
+  // }
+
+
+  // Training JS #7: if..else and ternary operator
+
+  // function saleHotdogs(n){
+  //   return n*(n<5?100:n<10?95:90);
+  // }
