cd52095a03c91f3ec50b3df6adf4c65996e5c0cc
Dostonbek <dostonbekabdusattarov9@gmail.com>
2022-11-24 23:10:45 +0500

    otherAngle

---
+++ 23-11/app.js
@@ -0,0 +1,36 @@
+// function multiply(arr, num){
+//   for (let i = 0; i < num.length; i++) {
+//     const element = arr[i];
+//     console.log(element)
+//   }
+// }
+
+// multiply(10,10)
+
+
+// function includer(arr, item){
+//  return Number(Math,sqrt(n))
+// }
+// console.log(includer([1,2,3,4], 3));
+
+
+
+// function otherAngle(a, b) {
+//   let total = 180;
+//    return console.log(total - (a+b))
+// }
+
+// otherAngle(29,10)
+
+
+
+
+// function positiveSum(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     return arr[i]+arr[i]
+//     if(arr[i].includes("-1") ){arr[i].replace(arr[i],0 )
+//   }
+//   return arr
+// }
+// }
+// console.log(positiveSum([1,2,3,4,5]));
